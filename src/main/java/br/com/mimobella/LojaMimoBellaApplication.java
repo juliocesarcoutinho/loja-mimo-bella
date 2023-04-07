8921d0383bc678215091ce86a0a50a72466d9533
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-04-07 20:47:04 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/mimobella/LojaMimoBellaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mimobella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaMimoBellaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaMimoBellaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mimobella/LojaMimoBellaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mimobella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaMimoBellaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
