88a73ff2a05691ab873428ef1f7de12d98037c30
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-04-07 21:35:36 -0300

    Configuração Inicial e Teste de entidade

---
+++ src/main/java/br/com/mimobella/model/MarcaProduto.java
@@ -0,0 +1,53 @@
+package br.com.mimobella.model;
+
+import com.sun.istack.NotNull;
+import org.aspectj.bridge.IMessage;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+    @Column(nullable = false, length = 120)
+    private String nomeDescricao;
+
+    /* ============================================================================================================== */
+
+    /* Getter and Setter */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return nomeDescricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.nomeDescricao = descricao;
+    }
+
+    /*Equal and hash code*/
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MarcaProduto that = (MarcaProduto) o;
+        return Objects.equals(id, that.id) && Objects.equals(nomeDescricao, that.nomeDescricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nomeDescricao);
+    }
+}
