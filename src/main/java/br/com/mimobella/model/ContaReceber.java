40f51000e58e794078a6f5b9d9f4f44dac0984dd
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-04-08 00:31:17 -0300

    Continuação da criação das entidades

---
+++ src/main/java/br/com/mimobella/model/ContaReceber.java
@@ -0,0 +1,121 @@
+package br.com.mimobella.model;
+
+import br.com.mimobella.enums.StatusContaReceber;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "conta_receber")
+@SequenceGenerator(name = "seq_conta_receber", sequenceName = "seq_conta_receber", allocationSize = 1, initialValue = 1)
+public class ContaReceber implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusContaReceber status;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dtVencimento;
+    @Temporal(TemporalType.DATE)
+    private Date dtPagamento;
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    @Column(nullable = false)
+    private BigDecimal valorDesconto;
+
+    @ManyToOne(targetEntity = Pessoa.class) /* Muitas contas para uma pessoa*/
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,
+            name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+
+    /* Getters and Setters*/
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusContaReceber getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusContaReceber status) {
+        this.status = status;
+    }
+
+    public Date getDtVencimento() {
+        return dtVencimento;
+    }
+
+    public void setDtVencimento(Date dtVencimento) {
+        this.dtVencimento = dtVencimento;
+    }
+
+    public Date getDtPagamento() {
+        return dtPagamento;
+    }
+
+    public void setDtPagamento(Date dtPagamento) {
+        this.dtPagamento = dtPagamento;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContaReceber that = (ContaReceber) o;
+
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
