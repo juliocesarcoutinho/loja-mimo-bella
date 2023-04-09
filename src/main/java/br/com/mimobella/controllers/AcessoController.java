df29aa4ea23334f2b595a6885e94657d33906e20
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-04-09 01:09:17 -0300

    Inicio dos testes

--- src/main/java/br/com/mimobella/controllers/AcessoController.java
+++ src/main/java/br/com/mimobella/controllers/AcessoController.java
@@ -1,17 +1,40 @@
 package br.com.mimobella.controllers;
 
 import br.com.mimobella.models.Acesso;
+import br.com.mimobella.repositories.AcessoRepository;
 import br.com.mimobella.services.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class AcessoController {
     @Autowired
     private AcessoService acessoService;
+    @Autowired
+    private AcessoRepository acessoRepository;
 
-    public Acesso salvarAcesso(Acesso acesso){
+    @ResponseBody
+    @PostMapping(value = "**/salvarAcesso")
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso){
 
-        return acessoService.save(acesso);
+        Acesso acessoSalvo = acessoService.save(acesso);
+
+        return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @PostMapping(value = "**/deleteAcesso")
+    public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso){
+
+        acessoRepository.deleteById(acesso.getId());
+
+        return new ResponseEntity<>("Acesso Removido ",HttpStatus.OK);
     }
 }
---
+++ src/main/java/br/com/mimobella/securitys/WebConfigSecurty.java
@@ -0,0 +1,24 @@
+package br.com.mimobella.securitys;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpSessionListener;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebConfigSecurty extends WebSecurityConfigurerAdapter implements HttpSessionListener {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso")
+                .antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
+        /*Ignora url para teste com postman */
+    }
+}
