23e3a1deeff248211d02e5bded586674373f5a60
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-04-09 21:46:37 -0300

    Testes com Junit pra validar alguns campos e algumas funções novas

--- src/test/java/br/com/mimobella/LojaMimoBellaApplicationTests.java
+++ src/test/java/br/com/mimobella/LojaMimoBellaApplicationTests.java
@@ -4,15 +4,18 @@ import br.com.mimobella.controllers.AcessoController;
 import br.com.mimobella.models.Acesso;
 import br.com.mimobella.repositories.AcessoRepository;
 import br.com.mimobella.services.AcessoService;
+import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = LojaMimoBellaApplication.class)
-class LojaMimoBellaApplicationTests {
+import java.util.List;
 
-//    @Autowired
-//    private AcessoRepository acessoRepository;
+@SpringBootTest(classes = LojaMimoBellaApplication.class)
+class LojaMimoBellaApplicationTests extends TestCase {
+
+    @Autowired
+    private AcessoRepository acessoRepository;
 //
 //    @Autowired
 //    private AcessoService acessoService;
@@ -24,8 +27,41 @@ class LojaMimoBellaApplicationTests {
     public void testCadastraAcesso(){
         Acesso acesso = new Acesso();
         acesso.setDescricao("ROLE_ADMIN");
-        acessoController.salvarAcesso(acesso);
+
+        assertEquals(true, acesso.getId() == null);
+
+        acesso = acessoController.salvarAcesso(acesso).getBody();
+
+        assertEquals(true, acesso.getId() > 0);
+
+        assertEquals("ROLE_ADMIN", acesso.getDescricao());
+
         System.out.println("Acesso cadastrado com Sucesso e o id é: " + acesso.getId());
+
+        /*Teste de carregamento*/
+        Acesso acesso1 = acessoRepository.findById(acesso.getId()).get();
+
+        assertEquals(acesso.getId(), acesso1.getId());
+
+        /*Teste de delete*/
+        acessoRepository.deleteById(acesso1.getId());
+
+        acessoRepository.flush();
+
+        Acesso acesso2 = acessoRepository.findById(acesso1.getId()).orElse(null);
+        assertEquals(true, acesso2 == null);
+
+        /*TESTE DE QUERY*/
+
+        acesso = new Acesso();
+        acesso.setDescricao("ROLE_GERENTE");
+        acesso = acessoController.salvarAcesso(acesso).getBody();
+
+        List<Acesso> acessos = acessoRepository.buscarAcessoDesc("GERENTE".toUpperCase().trim());
+
+        assertEquals(1, acessos.size());
+        acessoRepository.deleteById(acesso.getId());
+
     }
 
     @Test
