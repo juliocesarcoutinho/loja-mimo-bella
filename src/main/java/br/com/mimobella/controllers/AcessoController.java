46a0d7ae0d493acd1d293de083e28fc55541b9e1
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-04-11 19:58:06 -0300

    Feito  ajustes e testes

--- src/main/java/br/com/mimobella/controllers/AcessoController.java
+++ src/main/java/br/com/mimobella/controllers/AcessoController.java
@@ -7,10 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RestController
@@ -37,4 +36,33 @@ public class AcessoController {
 
         return new ResponseEntity<>("Acesso Removido ",HttpStatus.OK);
     }
+
+    @ResponseBody
+    @DeleteMapping(value = "**/deleteAcessoPorId/{id}")
+    public ResponseEntity<?> deleteAcessoPorId(@PathVariable("id") Long id){
+
+        acessoRepository.deleteById(id);
+
+        return new ResponseEntity<>("Acesso Removido ",HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "**/obterAcessoPorId/{id}")
+    public ResponseEntity<Acesso> obterAcessoPorId(@PathVariable("id") Long id){
+
+        Acesso acesso = acessoRepository.findById(id).get();
+
+        return new ResponseEntity<Acesso>(acesso,HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "**/obterAcessoPorDesc/{desc}")
+    public ResponseEntity<List<Acesso>> obterAcessoPorDesc(@PathVariable("desc") String desc){
+
+        List<Acesso> acesso = acessoRepository.buscarAcessoDesc(desc);
+
+        return new ResponseEntity<List<Acesso>>(acesso,HttpStatus.OK);
+    }
+
+
 }
