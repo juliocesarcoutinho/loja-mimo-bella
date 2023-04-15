d768c0ad11e2732365b440d8391a6ea5bb5e7cc7
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-04-14 21:55:00 -0300

    Teste de autenticação.

--- src/main/java/br/com/mimobella/LojaMimoBellaApplication.java
+++ src/main/java/br/com/mimobella/LojaMimoBellaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -11,9 +12,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = {"br.com.mimobella.repositories"}) /*Mapeamento dos pacotes dos Repositorios*/
 @EnableTransactionManagement /*Transações que serão feitas pelo projeto*/
 public class LojaMimoBellaApplication {
-
     public static void main(String[] args) {
+
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+
         SpringApplication.run(LojaMimoBellaApplication.class, args);
+
     }
 
 }
